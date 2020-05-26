761b2d8b86ca76c04c33f4d9fd411f50ff355e79
fernandofranca89 <63021925+fernandofranca89@users.noreply.github.com>
2020-05-25 22:08:04 -0300

    Create Extremamente Básico.java

---
+++ Extremamente Básico.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+     int A;
+     int B;
+     int X;
+     
+      Scanner sc = new Scanner(System.in);
+     
+     System.out.println("A");
+     A = sc.nextInt();
+     
+     System.out.println("B");
+     B = sc.nextInt();
+    
+     X = A+B;
+     
+     
+     System.out.println("x = " + X);
+ 
+    }
+ 
+}
